ff82459a18f83fbe4778fa86fb4542eb4dfde578
Marat Kuzakhmetov <maratkuzakhmetov@mail.ru>
2022-10-27 22:29:09 +0200

    Nav primary style is done

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  AiTwotoneHome, AiOutlineUser, AiFillHeart, AiOutlineMessage,
+  AiTwotoneHome, AiOutlineUser, AiOutlineHeart, AiOutlineMessage,
 } from 'react-icons/ai';
 import { GiNetworkBars } from 'react-icons/gi';
 import './Nav.scss';
 
 function Nav() {
+  const [style, setStyle] = useState('about');
   return (
     <nav className="nav">
-      <a href="!#">
+      <a href="!#" onClick={(e) => setStyle('about')} className={style === 'about' ? 'active' : 'nav__anchor'}>
         <AiTwotoneHome />
       </a>
-      <a href="!#">
+      <a href="!#" onClick={(e) => setStyle('experience')} className={style === 'experience' ? 'active' : 'nav__anchor'}>
         <AiOutlineUser />
       </a>
-      <a href="!#">
+      <a href="!#" onClick={(e) => setStyle('services')} className={style === 'services' ? 'active' : 'nav__anchor'}>
         <GiNetworkBars />
       </a>
-      <a href="!#">
-        <AiFillHeart />
+      <a href="!#" onClick={(e) => setStyle('portfolio')} className={style === 'portfolio' ? 'active' : 'nav__anchor'}>
+        <AiOutlineHeart />
       </a>
-      <a href="!#">
+      <a href="!#" onClick={(e) => setStyle('contact')} className={style === 'contact' ? 'active' : 'nav__anchor'}>
         <AiOutlineMessage />
       </a>
     </nav>

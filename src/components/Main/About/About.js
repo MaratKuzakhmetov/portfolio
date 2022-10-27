490a12b65e40cf3733cb320db1a2660151795be8
Marat Kuzakhmetov <maratkuzakhmetov@mail.ru>
2022-10-27 23:52:40 +0200

    primary style for About Component is done

--- src/components/Main/About/About.js
+++ src/components/Main/About/About.js
@@ -1,8 +1,31 @@
+/* eslint-disable max-len */
 import React from 'react';
+import './About.scss';
 
 function About() {
   return (
-    <div>About</div>
+    <section className="about">
+      <div className="about__intro">
+        <h5 className="about__intro-text about__text">Get to know</h5>
+      </div>
+      <div className="about__me">
+        <h2 className="about__me-text about__text">About me</h2>
+      </div>
+      <div className="about__details">
+        <div className="about__avatar">
+          <img src="./images/avatar.jpg" alt="img" className="about__avatar-img" />
+        </div>
+        <div className="about__information">
+          <p className="about__information-text">
+            I do front-end development. I like to develop and learn something new. And this is the main reason why I chose the IT field. There is no limit to development in IT.
+          </p>
+          <p className="about__information-text">
+            I love teamwork as well as solo projects! I am an extroverted introvert, so I am productive in any work environment.
+          </p>
+          <a href="#!" className="header__button button">Write me</a>
+        </div>
+      </div>
+    </section>
   );
 }
 

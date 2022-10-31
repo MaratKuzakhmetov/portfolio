44023f3074e686620ffc01919a96717e5c80edb8
Marat Kuzakhmetov <maratkuzakhmetov@mail.ru>
2022-10-31 22:55:11 +0100

    Contact Component 50% of basic style is done

--- src/components/Main/Contact/Contact.js
+++ src/components/Main/Contact/Contact.js
@@ -1,8 +1,48 @@
 import React from 'react';
+import './Contact.scss';
+import { AiOutlineMail, AiOutlineWhatsApp } from 'react-icons/ai';
+import { BsTelegram } from 'react-icons/bs';
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <section className="contact">
+      <div className="contact__intro">
+        <h5 className="contact__intro-text contact__text">Get In Touch</h5>
+      </div>
+      <div className="contact__me">
+        <h2 className="contact__me-text contact__text">Contact Me</h2>
+      </div>
+      <div className="contact__wrapper">
+        <div className="contact__options">
+          <article className="contact__option">
+            <AiOutlineMail className="contact__icon" />
+            <h4 className="contact__title">Email</h4>
+            <h5 className="contact__info">kuzakhmetovmarat@gmail.com</h5>
+            <a href="mailto:kuzakhmetovmarat@gmail.com" className="contact__send">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsTelegram className="contact__icon" />
+            <h4 className="contact__title">Messenger</h4>
+            <h5 className="contact__info">Telegram</h5>
+            <a href="https://t.me/Marat_Kuzakhmetov" className="contact__send">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsTelegram className="contact__icon" />
+            <h4 className="contact__title">Messenger</h4>
+            <h5 className="contact__info">WhatsApp</h5>
+            <a href="https://api.whatsapp.com/send?phone=4915736524376" className="contact__send">Send a message</a>
+          </article>
+        </div>
+        <div className="contact__message">
+          <form>
+            <input type="name" name="name" placeholder="Your Full Name" target="blanc" required />
+            <input type="email" name="email" placeholder="Your Email" required />
+            <textarea name="message" rows="7" placeholder="Your Message" required />
+            <button type="submit">Send Message</button>
+          </form>
+        </div>
+      </div>
+    </section>
   );
 }
 
